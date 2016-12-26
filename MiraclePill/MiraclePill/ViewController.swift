d7931c85083dc473037be77fb6f457bd2919ec98
isa <isaliangyiyun@hotmail.com>
2016-12-26 11:26:50 +0800

    create miracle pill ui project

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Yiyun Liang on 2016-12-26.
+//  Copyright © 2016 Yiyun Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.purple
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
