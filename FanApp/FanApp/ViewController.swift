9718fcc16e556b8b9dc856906e4087caa14a088e
isa <isaliangyiyun@hotmail.com>
2016-12-27 18:14:23 +0800

    add new fan app project

---
+++ FanApp/FanApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FanApp
+//
+//  Created by Yiyun Liang on 2016-12-27.
+//  Copyright © 2016 Yiyun Liang. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Some mini Swift projects I completed while I was learning Swift online.
 
++ [Fan App](https://github.com/Yiyun-Liang/Swift-Miniprojects/tree/master/FanApp)
+
 + [Miracle Pill](https://github.com/Yiyun-Liang/Swift-Miniprojects/tree/master/MiraclePill)
+
++ [Swapping Screens](https://github.com/Yiyun-Liang/Swift-Miniprojects/tree/master/SwappingScreens)
 
 + [UI Scroll View](https://github.com/Yiyun-Liang/Swift-Miniprojects/tree/master/UIScrollView)
