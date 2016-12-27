3f2467a9a13e145ad71f4541033e9bda4d9457ea
isa <isaliangyiyun@hotmail.com>
2016-12-27 18:59:28 +0800

    create table view and whole ui

---
+++ FanApp/FanApp/VideoVC.swift
@@ -0,0 +1,19 @@
+//
+//  VideoVC.swift
+//  FanApp
+//
+//  Created by Yiyun Liang on 2016-12-27.
+//  Copyright © 2016 Yiyun Liang. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
