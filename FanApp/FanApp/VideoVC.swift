8bd8956e3984956f3167b76b24e82e67b3bdb808
isa <isaliangyiyun@hotmail.com>
2016-12-28 18:48:10 +0800

    show youtube videos in a web view

--- FanApp/FanApp/VideoVC.swift
+++ FanApp/FanApp/VideoVC.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class VideoVC: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    // there is another way of using web view, all programmatically
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    private var _fanApp: FanApp!
+    
+    // is called before viewDidLoad()
+    var fanApp: FanApp {
+        get {
+            return _fanApp
+        } set {
+            _fanApp = newValue
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        titleLbl.text = fanApp.videoTitle
+        webView.loadHTMLString(fanApp.videoUrl, baseURL: nil)  // fanApp.videoUrl is an iframe
     }
     
 }
